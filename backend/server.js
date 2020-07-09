337c0fe709dab6c3bdeb560f93c59a053b86d929
Dmitriy <danny88100@gmail.com>
2020-07-09 16:05:01 -0400

    Created front-end of App

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,11 @@ connection.once('open', () => {
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
+const servicesRouter = require('./routes/services');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+app.use('/services', servicesRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
